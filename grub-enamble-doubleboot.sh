870ed35b5bbe96c3615f2da5f5b9d6f04a910dd3
Angel Darco <angel92-eme@hotmail.com>
2023-05-28 12:05:26 -0500

    script to enable double boot arch and windows

---
+++ grub-enamble-doubleboot.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+#detect others os
+#sudo os-prober
+
+#update os preferences 
+#sudo nano /etc/default/grub
+#GRUB_DISABLE_OS_PROBER=false
+
+#update grub
+#sudo grub-mkconfig -o /boot/grub/grub.cfg
+#
